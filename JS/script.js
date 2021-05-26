261ad5d80ea171348aebf5dd27bb25fa2a86e074
solangeguzman <solangeguzmanduchen@gmail.com>
2021-05-26 15:45:40 +0200

    prova JavaScript

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+document.getElementById('IDtittle').innerHTML='HI, WELCOME TO MY PAGE!!';
+var x=20
+var y=35
+var Arancione= x+y;
+console.log('Amazing!');
+document.getElementById('risposta-giusta').innerHTML='Arancione!';
+alert('Se il commando risponde vuol dire che è scritto giusto!');
